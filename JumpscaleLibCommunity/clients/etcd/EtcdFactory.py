f7053719c99ae514b14ad7fc15a608197b17608b
despiegk <kristof@incubaid.com>
2018-08-08 10:15:01 +0200

    digitalme

---
+++ JumpscaleLibCommunity/clients/etcd/EtcdFactory.py
@@ -0,0 +1,12 @@
+from jumpscale import j
+from .EtcdClient import EtcdClient
+
+
+JSConfigFactoryBase = j.tools.configmanager.base_class_configs
+
+
+class EtcdFactory(JSConfigFactoryBase):
+
+    def __init__(self):
+        self.__jslocation__ = "j.clients.etcd"
+        super().__init__(child_class=EtcdClient)
